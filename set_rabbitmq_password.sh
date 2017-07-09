0b99ca6a96f5fb679018b80b38e8dcaa5df18579
nesty <rajitha.wijayaratne@gmail.com>
2017-07-09 15:37:04 +1000

    takes credentials from app.nest

--- set_rabbitmq_password.sh
+++ set_rabbitmq_password.sh
@@ -5,8 +5,9 @@ if [ -f /.rabbitmq_password_set ]; then
 	exit 0
 fi
 
-PASS=${RABBITMQ_PASS:-$(pwgen -s 12 1)}
-USER=${RABBITMQ_USER:-"admin"}
+PASS=${RABBITMQ_PASS:-$(jq -r ".services_password" /var/app/app.json)}
+USER=${RABBITMQ_USER:-$(jq -r ".tag" /var/app/app.json)}
+
 _word=$( [ ${RABBITMQ_PASS} ] && echo "preset" || echo "random" )
 echo "=> Securing RabbitMQ with a ${_word} password"
 cat > /etc/rabbitmq/rabbitmq.config <<EOF
@@ -21,14 +22,6 @@ touch /.rabbitmq_password_set
 echo "========================================================================"
 echo "You can now connect to this RabbitMQ server using, for example:"
 echo ""
-
-if [ ${_word} == "random" ]; then
-    echo "    curl --user $USER:$PASS http://<host>:<port>/api/vhosts"
-    echo ""
-    echo "Please remember to change the above password as soon as possible!"
-else
-    echo "    curl --user $USER:<RABBITMQ_PASS> http://<host>:<port>/api/vhosts"
-    echo ""
-fi
-
+echo "    curl --user $USER:<RABBITMQ_PASS> http://<host>:<port>/api/vhosts"
+echo ""
 echo "========================================================================"
